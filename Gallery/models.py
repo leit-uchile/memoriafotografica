d888a5e3b035e457e18d725567f49ca38817a650
victoriabollo <m.victoria1263@gmail.com>
2018-07-07 12:46:46 -0400

    Agregado modelo Photo, Album.

--- Gallery/models.py
+++ Gallery/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+
+class Photo(models.Model):
+
+    name = models.CharField(max_length=40)  # type: str
+    description = models.TextField()
+    image = models.ImageField()
+    uploadDate = models.DateTimeField('date published', default=datetime.now, blank=True)
+    tags = models.ManyToManyField()
+
+    def __str__(self):
+        return "Photo " + self.name
+
+class Album(models.Model):
+    
+    name = models.CharField(max_length=40)
+    pictures = models.ManyToManyField()
+
+    def __str__(self):
+        return "Album " + self.name
