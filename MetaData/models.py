f3ebfdc38a2fc8444693a109a57413b828b728e9
Darío Cáceres <dignaciocaceres@gmail.com>
2018-07-07 12:48:31 -0400

    Agregados modelos User, Metadata

--- MetaData/models.py
+++ MetaData/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Metadata(models.Model):
+    name = models.CharField(max_length=40)
+
+
+    Agregado modelo Photo, Album
--- Users/models.py
+++ Users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+import django.contrib.auth.models as django_md
+from Gallery.models import Album
+
+class User(models.Model):
+    user = models.OneToOneField(django_md.User, on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to='user/avatar')
+    albums = models.ManyToManyField(Album, on_delete=models.CASCADE)
