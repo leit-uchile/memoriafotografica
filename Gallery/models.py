7d36d7d44c3d4e23b5175f984bb108bd69a73870
victoriabollo <m.victoria1263@gmail.com>
2018-07-07 16:22:28 -0400

    Agregado modelos al admin

--- Gallery/models.py
+++ Gallery/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from datetime import datetime
+from MetaData.models import Metadata
 # Create your models here.
 
 class Photo(models.Model):
@@ -8,15 +9,15 @@ class Photo(models.Model):
     description = models.TextField()
     image = models.ImageField()
     uploadDate = models.DateTimeField('date published', default=datetime.now, blank=True)
-    tags = models.ManyToManyField()
+    tags = models.ManyToManyField(Metadata)
 
     def __str__(self):
         return "Photo " + self.name
 
 class Album(models.Model):
-    
+
     name = models.CharField(max_length=40)
-    pictures = models.ManyToManyField()
+    pictures = models.ManyToManyField(Photo)
 
     def __str__(self):
         return "Album " + self.name
--- Users/models.py
+++ Users/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 # Create your models here.
 import django.contrib.auth.models as django_md
-from Gallery.models import Album
+from Gallery.models import Album, Photo
 
 class User(models.Model):
     user = models.OneToOneField(django_md.User, on_delete=models.CASCADE)
-    avatar = models.ImageField(upload_to='user/avatar')
-    albums = models.ManyToManyField(Album, on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to='user/avatar', blank=True)
+    albums = models.ManyToManyField(Album, blank =True)
+    photos = models.ManyToManyField(Photo, blank = True)
