58c2871fa11e28e30f63eb0475b411eb9688614d
dariopalma <spam.dario@gmail.com>
2018-07-21 11:36:44 -0400

    Se agrega loggin basico sin redux ni llamadas a la API

--- static/src/pages/Login.js
+++ static/src/pages/Login.js
@@ -1,14 +1,44 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+
 
 class Login extends Component{
 
     constructor(){
         super();
+        this.state = {
+            username : "",
+            password : "",
+        }
+    }
+
+    updateUserName = e => {this.setState({username : e.target.value})}
+
+    updatePassword = e => {this.setState({password : e.target.value})}
+
+    sendForm = function(){
+        alert("Aun no implementado")
+        console.log(this.state)
     }
 
     render(){
         return(
-            <h1>Login</h1>
+            <div>
+                <form onSubmit={this.sendForm}>
+                    <h1>Login</h1>
+                    <fieldset>
+                        <p>
+                            <label htmlFor="username">Nombre de usuario</label>
+                            <input type="text" onChange={this.updateUserName}/>
+                        </p>
+                        <p>
+                            <label htmlFor="username">Nombre de usuario</label>
+                            <input type="password" onChange={this.updatePassword}/>
+                        </p>
+                    </fieldset>
+                    <button type="submit">Entrar</button>
+                </form>
+            </div>
         );
     }
 
