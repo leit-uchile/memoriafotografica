4bbbaf8fe860a4eb48c6ff9f79fd6f8ae21a423c
IsaiasVenegas <isaias.venegas@ing.uchile.cl>
2018-07-21 13:33:55 -0400

    Creacion componentes Photo y Gallery separados de App

--- static/src/components/Photo.js
+++ static/src/components/Photo.js
@@ -6,10 +6,11 @@ class Photo extends Component {
         this.props = props;
     }
     render() {
+        const imgStyle = {height: '100px', width: '100px'};
         return(
-            <div>
+            <div className='col-lg-4'>
                 <h3>{this.props.name}</h3>
-                <img src={this.props.url} alt={this.props.name}/>
+                <img style={imgStyle} src={this.props.url} alt={this.props.name}/>
                 <p>{this.props.desc}</p>
                 <ul>
                     {this.props.tags.map( (el) => {
--- static/src/pages/Home.js
+++ static/src/pages/Home.js
@@ -4,21 +4,21 @@ import Gallery from '../components/Gallery';
 let imgs = [
     {
         name: "img1",
-        url: "",
+        url: "https://mott.pe/noticias/wp-content/uploads/2016/11/Janette-Asche.jpg",
         tags: ["tag1","tag2"],
         // Lorem ipsum Dolor Sit Amet
         desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed feugiat leo vitae felis iaculis, sit amet placerat tellus tincidunt. Nunc at vestibulum elit. Vivamus id ipsum eros. Curabitur pulvinar nulla eu magna euismod iaculis. Nulla facilisi. Vestibulum euismod augue vel semper condimentum. Mauris laoreet, quam quis finibus malesuada, tellus tellus ultrices odio, quis commodo nulla nulla vestibulum arcu. Nullam aliquet, quam id porttitor consectetur, arcu velit congue arcu, non sollicitudin nunc dolor id enim. Nunc ultricies eget mauris id dapibus. Praesent magna lorem, lacinia id tristique vel, fringilla eget urna. Sed velit elit, rhoncus at ligula ac, porta dapibus nunc. Morbi eu nulla vel lectus porta egestas sit amet vitae dui. "
     },
     {
         name: "img2",
-        url: "",
+        url: "https://i.pinimg.com/736x/27/8a/2e/278a2e7cdc2f3d04df099a3802b301f1--chile-villa.jpg",
         tags: ["tag2","tag3"],
         // Lorem ipsum Dolor Sit Amet
         desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed feugiat leo vitae felis iaculis, sit amet placerat tellus tincidunt. Nunc at vestibulum elit. Vivamus id ipsum eros. Curabitur pulvinar nulla eu magna euismod iaculis. Nulla facilisi. Vestibulum euismod augue vel semper condimentum. Mauris laoreet, quam quis finibus malesuada, tellus tellus ultrices odio, quis commodo nulla nulla vestibulum arcu. Nullam aliquet, quam id porttitor consectetur, arcu velit congue arcu, non sollicitudin nunc dolor id enim. Nunc ultricies eget mauris id dapibus. Praesent magna lorem, lacinia id tristique vel, fringilla eget urna. Sed velit elit, rhoncus at ligula ac, porta dapibus nunc. Morbi eu nulla vel lectus porta egestas sit amet vitae dui. "
     },
     {
         name: "img3",
-        url: "",
+        url: "https://enviajes.cl/wp-content/uploads/2014/10/Chile-Fotos-Catedral-de-Marmol.jpg",
         tags: ["tag1","tag3"],
         // Lorem ipsum Dolor Sit Amet
         desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed feugiat leo vitae felis iaculis, sit amet placerat tellus tincidunt. Nunc at vestibulum elit. Vivamus id ipsum eros. Curabitur pulvinar nulla eu magna euismod iaculis. Nulla facilisi. Vestibulum euismod augue vel semper condimentum. Mauris laoreet, quam quis finibus malesuada, tellus tellus ultrices odio, quis commodo nulla nulla vestibulum arcu. Nullam aliquet, quam id porttitor consectetur, arcu velit congue arcu, non sollicitudin nunc dolor id enim. Nunc ultricies eget mauris id dapibus. Praesent magna lorem, lacinia id tristique vel, fringilla eget urna. Sed velit elit, rhoncus at ligula ac, porta dapibus nunc. Morbi eu nulla vel lectus porta egestas sit amet vitae dui. "
@@ -39,7 +39,7 @@ class Home extends Component{
 
     render(){
         return(
-            <div><h1>Gallery Home</h1>
+            <div className='container'><h1>Gallery Home</h1>
             <Gallery imageList={this.props.gallery} />
             </div>
 
