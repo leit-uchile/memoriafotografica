720762a85046f422c99572d6ff3c353bc1d7a5d0
victoria <m.victoria1263@gmail.com>
2018-10-13 14:29:44 -0300

    Modifica modelo de usuario, falta probar si funciona. Agrega permisos a Photos y Comentarios

--- Gallery/models.py
+++ Gallery/models.py
@@ -3,13 +3,26 @@ from datetime import datetime
 from django.utils.text import slugify
 # Create your models here.
 
-class Photo(models.Model):
 
+
+
+class Photo(models.Model):
+    PERMISSION_CHOICES = (
+        ('a', 'Hola'),
+        ('b', 'Hello'),
+        ('c', 'Bonjour'),
+        ('d', 'Boas'),
+    )
+)
     image = models.ImageField()
     uploadDate = models.DateTimeField('date published', default=datetime.now, blank=True)
+    title = models.CharField(_('Título'), max_length = 30)
     thumbnail = models.ImageField(blank=True, null=True)
     approved = models.BooleanField(default=False)
     #keywords = models.ManyToManyField(MetadataKeyword)
+    censure = models.BooleanField(default = False)
+    permission = models.CharField(max_length=6, choices=PERMISSION_CHOICES)  #¿Elige varios?
+
 
     def __str__(self):
         try:
@@ -25,3 +38,9 @@ class Album(models.Model):
 
     def __str__(self):
         return "Album " + self.name
+
+class Comment(models.Model):
+    contenido = models.ManyToManyField(Photo)
+    censure = models.BooleanField()
+    def __str__(self):
+        return "Comentario"
--- Users/models.py
+++ Users/models.py
@@ -1,19 +1,68 @@
+from __future__ import unicode_literals
 from django.db import models
-
-# Create your models here.
 import django.contrib.auth.models as django_md
-from Gallery.models import Album, Photo
+from django.core.mail import send_mail
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.utils.translation import ugettext_lazy as _
 
-class User(models.Model):
+from .managers import UserManager
+from Gallery.models import Album, Photo, Comment
+
+class User(AbstractBaseUser, PermissionsMixin):
 
     USER_TYPE_CHOICES = (
-      (1, 'normal'),
-      (2, 'curator')
+        (1, 'colaborator'),
+        (2, 'curator'),
+        (3, 'admin')
     )
+    ROL_TYPE_CHOICES = (
+        (1, 'Alumno'),
+        (2, 'Ex-Alumno'),
+        (3, 'Académico'),
+        (4, 'Ex-Académico'),
+        (5, 'Funcionario'),
+        (6, 'Externo')
+    )
+    email = models.EmailField(_('email address'), unique=True)
+    first_name = models.CharField(_('first name'), max_length=30)
+    last_name = models.CharField(_('last name'), max_length=30)
+    date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
+    is_active = models.BooleanField(_('active'), default=True)      #Habilitado
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    deleted = models.BooleanField(_('deleted'), default = False)    #Eliminado
+    generation = models.CharField(_('generation'), max_length = 5, blank = True)
+    albums = models.ManyToManyField(Album, blank=True)
+    photos = models.ManyToManyField(Photo, blank=True)
+    user_type = models.PositiveSmallIntegerField(choices=USER_TYPE_CHOICES, blank = 1)
+    rol_type = models.PositiveSmallIntegerField(choices=ROL_TYPE_CHOICES, blank=6)
+    comments = models.ManyToManyField(Comment, blank = True)
+    objects = UserManager()
 
-    user = models.OneToOneField(django_md.User, on_delete=models.CASCADE)
-    avatar = models.ImageField(upload_to='user/avatar', blank=True)
-    albums = models.ManyToManyField(Album, blank =True)
-    photos = models.ManyToManyField(Photo, blank = True)
-    user_type = models.PositiveSmallIntegerField(choices=USER_TYPE_CHOICES)
-    
+    USERNAME_FIELD = 'email', 'first name', 'lats name'
+    REQUIRED_FIELDS = ['email', 'first name', 'last name']
+'''
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+    def get_full_name(self):
+        '''
+        Returns the first_name plus the last_name, with a space in between.
+        '''
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def get_short_name(self):
+        '''
+        Returns the short name for the user.
+        '''
+        return self.first_name
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        '''
+        Sends an email to this User.
+        '''
+        send_mail(subject, message, from_email, [self.email], **kwargs)
+
+'''
