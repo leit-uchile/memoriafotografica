1245686efd1a5c5fc383e1210a5789d6da6324e8
victoria <m.victoria1263@gmail.com>
2018-10-27 13:35:18 -0300

    agrega comments en api

--- Gallery/api.py
+++ Gallery/api.py
@@ -4,31 +4,44 @@ from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 from .models import Photo, Album
-from .serializers import CreatePhotoSerializer, PhotoSerializer, AlbumSerializer
+from .serializers import CreatePhotoSerializer, PhotoSerializer, AlbumSerializer, CommentSerializer
 from MetaData.models import MetadataTitle, MetadataDescription
 from .permissions import *
 
+#Metadata¿?
 def add_title_description(request, p_id):
     if request.method == 'POST':
         t = request.POST.get('title')
         d = request.POST.get('description')
-        title = MetadataTitle.objects.create(title = t, description = t.lower(), photo = Photo.objects.get(pk = p_id))
-        description = MetadataDescription.objects.create(description = d,  photo = Photo.objects.get(pk = p_id))
-
-
+        title = MetadataTitle.objects.create(title=t, description=t.lower(), photo=Photo.objects.get(pk=p_id))
+        description = MetadataDescription.objects.create(description=d, photo=Photo.objects.get(pk=p_id))
 
 
 class PhotoUploadAPI(generics.GenericAPIView):
-    permission_classes = [IsOwnerOrReadOnly,]
+    permission_classes = [IsOwnerOrReadOnly, ]
     parser_classes = (MultiPartParser, FormParser)
     serializer_class = CreatePhotoSerializer
 
     def post(self, request, *args, **kwargs):
         print(request.user.id)
-        photo_serializer = self.get_serializer(data = request.data)
+        photo_serializer = self.get_serializer(data=request.data)
         if photo_serializer.is_valid():
             photo_serializer.save()
-            #add_title_description(request, photo_serializer.data['id'])
+            # add_title_description(request, photo_serializer.data['id'])
             return Response(photo_serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(photo_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class CommentUploadAPI(generics.GenericAPIView):
+    permissions_classes = [IsOwnerOrReadOnly]
+    parser_classes = (MultiPartParser, FormParser)
+
+    def post(self, request, *args, **kwargs):
+        comment_serializer = CommentSerializer(data = request.data)
+        if comment_serializer.is_valid():
+            comment_serializer.save()
+            return Response(comment_serializer.data,  status = status.HTTP_201_CREATED)
+        else:
+            return Response(comment_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
--- Gallery/endpoints.py
+++ Gallery/endpoints.py
@@ -2,9 +2,8 @@ from .api import *
 from django.urls import re_path, include
 
 
-
-
-
 urlpatterns = [
-    re_path("^gallery/upload/$", PhotoUploadAPI.as_view()),
+    re_path("^gallery/upload-photo/$", PhotoUploadAPI.as_view()),
+    re_path('^gallery/upload-comment/$', CommentUploadAPI.as_view())
     ]
+
