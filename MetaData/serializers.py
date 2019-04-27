e2dd6ad7cc487b266283d70a5814d30de0f14f7f
Victoria Bollo <m.victoria1263@gmail.com>
2019-04-27 10:10:48 -0400

    añade serializers Metada

--- MetaData/serializers.py
+++ MetaData/serializers.py
@@ -8,47 +8,38 @@ from rest_framework import serializers
 from .models import *
 from Gallery.models import Photo
 
-class MetadataSerializerTitle(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(required=False, allow_blank=True, max_length=100)
-    description = serializers.CharField(required = False, allow_blank= True)
 
+
+
+class IPTCKeywordSerializer(serializers.Serializer):
+    class Meta:
+        model = IPTCKeyword
+        fields = '__all__'
     def create(self, validated_data):
 
-        return MetadataTitle.objects.create(**validated_data)
+        return IPTCKeyword.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
 
-        instance.title = validated_data.get('title', instance.title)
-        instance.description = validated_data.get('description', instance.title)
+        instance.name = validated_data.get('name', instance.name)
+        instance.definition = validated_data.get('definition', instance.definition)
+        instance.help_text = validated_data.get('help_text', instance.help_text)
         instance.save()
         return instance
 
 
-class MetadataSerializerDescription(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    description = serializers.CharField(required = False, allow_blank= True)
-
+class MetadataSerializer(serializers.Serializer):
+   class Meta:
+       model = Metadata
+       fields = '__all__'
 
     def create(self, validated_data):
-
-        return MetadataDescription.objects.create(**validated_data)
+        return MetadataSerializer.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-
-        instance.description = validated_data.get('description', instance.title)
+        instance.value = validated_data.get('value', instance.value)
+        instance.metadata = validated_data.get('metadata', instance.metadata)
         instance.save()
         return instance
 
-class MetadataSerializerKeyword(serializers.Serializer):
-      id = serializers.IntegerField(read_only=True)
-      keyword = serializers.CharField()
-      def create(self, validated_data):
-          return MetadataKeyword.objects.create(**validated_data)
-
-      def update(self, instance, validated_data):
-          instance.keyword = validated_data.get('keyword', instance.title)
-
-          instance.save()
-          return instance
 
