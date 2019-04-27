601e3479918a85e8169e317c1e8d88b821b20b0c
Victoria Bollo <m.victoria1263@gmail.com>
2019-04-27 09:20:20 -0400

    add models in Metadata

--- MemoriaFotografica/settings.py
+++ MemoriaFotografica/settings.py
@@ -129,13 +129,13 @@ USE_TZ = True
 # ReactJS
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 
 # Media files
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
 
