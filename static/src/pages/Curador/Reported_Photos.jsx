5e7babf7cb01e978cb606ee470a463bad9982d32
IsaiasVenegas <isaias.venegas@ing.uchile.cl>
2019-05-28 18:17:12 -0400

    Se cambia visualmente paginas del Curador

--- static/src/pages/Curador/Reported_Photos.jsx
+++ static/src/pages/Curador/Reported_Photos.jsx
@@ -5,39 +5,57 @@ import {Button, ButtonGroup, Row, Col, Container} from 'reactstrap';
 var imgs = [
     {
         name: "img1",
-        url: "https://www.smashbros.com/wiiu-3ds/images/character/koopa/main.png",
+        url: "https://www.ssbwiki.com/images/thumb/2/2b/Isabelle_SSBU.png/250px-Isabelle_SSBU.png",
         tags: ["tag1","tag2"],
         // Lorem ipsum Dolor Sit Amet
         desc: "desc1",
-        reason: "me cae mal",
-        state: "marked"
+        reason: "Foto de perfil inapropiada",
+        state: "uploaded"
     },
     {
         name: "img2",
-        url: "https://i.kym-cdn.com/photos/images/newsfeed/001/141/079/8f3.jpg",
+        url: "https://www.bsn.eu/wp-content/uploads/2016/12/user-icon-image-placeholder.jpg",
         tags: ["tag2","tag3"],
         // Lorem ipsum Dolor Sit Amet
         desc: "desc2",
-        reason: "foto inapropiada",
-        state: "marked"
+        reason: "Bot",
+        state: "uploaded"
     },
     {
         name: "img3",
-        url: "https://vignette.wikia.nocookie.net/fantendo/images/b/b6/Waluigi_Artwork_-_Super_Smash_Bros._Brawl.png/revision/latest?cb=20160503203605",
+        url: "https://www.bsn.eu/wp-content/uploads/2016/12/user-icon-image-placeholder.jpg",
         tags: ["tag2","tag3"],
         // Lorem ipsum Dolor Sit Amet
         desc: "desc3",
-        reason: "niguna",
-        state: "marked"
+        reason: "Comentarios ofensivos",
+        state: "uploaded"
     },
     {
         name: "img4",
-        url: "https://vignette.wikia.nocookie.net/fantendo/images/b/b6/Waluigi_Artwork_-_Super_Smash_Bros._Brawl.png/revision/latest?cb=20160503203605",
+        url: "https://www.bsn.eu/wp-content/uploads/2016/12/user-icon-image-placeholder.jpg",
         tags: ["tag2","tag3"],
         // Lorem ipsum Dolor Sit Amet
         desc: "desc4",
-        reason: "foto duplicada",
-        state: "marked"
+        reason: "Usuario duplicado",
+        state: "uploaded"
+    },
+    {
+        name: "img5",
+        url: "https://www.bsn.eu/wp-content/uploads/2016/12/user-icon-image-placeholder.jpg",
+        tags: ["tag2","tag3"],
+        // Lorem ipsum Dolor Sit Amet
+        desc: "desc5",
+        reason: "Usuario duplicado",
+        state: "uploaded"
+    },
+    {
+        name: "img6",
+        url: "https://www.bsn.eu/wp-content/uploads/2016/12/user-icon-image-placeholder.jpg",
+        tags: ["tag2","tag3"],
+        // Lorem ipsum Dolor Sit Amet
+        desc: "desc6",
+        reason: "Usuario duplicado",
+        state: "uploaded"
     }
 ];
 
@@ -82,7 +100,7 @@ class Reported_Photos extends Component{
         var latest = []
         for (var i = 1; i < 4 && i < imgs.length; i++) {
             latest.push(
-                <Photo name={this.state.photos[i].name} url={this.state.photos[i].url} tags={this.state.photos[i].tags} state={this.state.photos[i].state}/>
+                <Photo name={this.state.photos[i].name} url={this.state.photos[i].url} tags={this.state.photos[i].tags} state={this.state.photos[i].state} width='150px' style={styles.latest}/>
             )
         }
         var actually = ''
@@ -92,29 +110,49 @@ class Reported_Photos extends Component{
         }
         else{
             actually= <Photo name={this.state.photos[0].name} url={this.state.photos[0].url} tags={this.state.photos[0].tags}
-                       desc={this.state.photos[0].desc} state={this.state.photos[0].state} height="400px"/>
-            reason = <p>Motivo del reporte: {this.state.photos[0].reason}</p>
+                       desc={this.state.photos[0].desc} state={this.state.photos[0].state} style={styles.actual} height='350px'/>
+            reason = <p style={{textAlign:'center'}}>Motivo del reporte: {this.state.photos[0].reason}</p>
         }
         return(
             <Container>
-                <Row>
-                    <Col sm={8}>
+            <Row>
+                <Col>
+                    <h2>Usuarios reportados</h2>
+                </Col>
+            </Row>
+            <div style={{backgroundColor: 'rgb(245,245,245)', border:'1px solid rgb(156,158,159)', borderRadius:'10px', marginTop: '2em', padding: '2em 0'}}>    
+                <Row>   
+                    <Col md='8'>
                         {actually}
                         {reason}
-                        {imgs.length === 0 ? null : 
-                        <ButtonGroup>
-                            <Button onClick={this.updatePhotoState}>Dar de baja</Button>
-                            <Button onClick={this.removePhoto}>Descartar reporte</Button>
-                        </ButtonGroup>}
+                        {imgs.length===0 ? null: 
+                            <Row>
+                                <ButtonGroup style={{marginTop:'10px',marginLeft:'auto',marginRight:'auto'}}>
+                                    <Button color="danger" onClick={this.updatePhotoState}>Eliminar</Button>
+                                    <Button color="secondary" onClick={this.removePhoto}>Descartar reporte</Button>
+                                </ButtonGroup>
+                            </Row>
+                        }
                     </Col>
-                    <Col sm={4}>
+                    <Col md='4'>
                         {latest}
                     </Col>
                 </Row>
-            </Container>
+            </div>
+        </Container>
         );
     }
-
+}
+const styles={
+    latest:{
+        width:'150px',
+        marginBottom: '20px',
+        boxShadow: '5px 5px 5px #3c4145',
+    },
+    actual:{
+        height:'350px',
+        textAlign:'center'
+    }
 }
 Reported_Photos.props = {
     gallery: imgs
