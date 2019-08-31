45191b80bd5eda946f32b7745c76342c2392c346
dario617 <spam.dario@gmail.com>
2019-08-31 11:42:23 -0400

    Nuevos logos y sesion expira

--- static/src/reducers/auth.js
+++ static/src/reducers/auth.js
@@ -1,27 +1,36 @@
-const initialState = {
-  token: localStorage.getItem("token"),
-  isAuthenticated: (localStorage.getItem("isAuth") === "true"),
-  isLoading: true,
-  user: JSON.parse(localStorage.getItem("user")),
-  errors: {},
-};
+const baseState = {token: null, isAuthenticated: false, isLoading: true, user: null, errors: {}};
+
+// Check on startup if timestamp is too old
+// and then remove the cookie if necessary
+const initialState = 
+  localStorage.getItem("isAuth") === null ? baseState : 
+  (new Date()).getTime() - JSON.parse(localStorage.getItem("isAuth")).logginDate > 86000000 ? baseState : 
+  {
+    token: localStorage.getItem("token"),
+    isAuthenticated: true,
+    isLoading: true,
+    user: JSON.parse(localStorage.getItem("user")),
+    errors: {},
+  };
 
 
 export default function auth(state=initialState, action) {
   // TODO: Actually load the info in a different State!
+  let logginDate = new Date();
+
   switch (action.type) {
     case 'USER_LOADED':
       return {...state, isAuthenticated: true, isLoading: false, user: action.user};
 
-    case 'LOGIN_SUCCESSFUL':
+    case 'LOGIN_SUCCESSFUL':    
       localStorage.setItem("token", action.data.token);
-      localStorage.setItem("isAuth", true);
+      localStorage.setItem("isAuth", JSON.stringify({"loggedIn": true, "timeSet": logginDate.getTime()}));
       localStorage.setItem("user", JSON.stringify(action.data.user));
       return {...state, ...action.data, isAuthenticated: true, isLoading: false, errors: null};
 
     case 'REGISTRATION_SUCCESSFUL':
       localStorage.setItem("token", action.data.token);
-      localStorage.setItem("isAuth", true);
+      localStorage.setItem("isAuth", JSON.stringify({"loggedIn": true, "timeSet": logginDate.getTime()}));
       localStorage.setItem("user", JSON.stringify(action.data.user));
       return {...state, ...action.data, isAuthenticated: true, isLoading: false, errors: null};
 
@@ -34,7 +43,7 @@ export default function auth(state=initialState, action) {
       return {...state, errors: action.data}
     case 'LOGOUT_SUCCESSFUL':
       localStorage.removeItem("token");
-      localStorage.setItem("isAuth", false);
+      localStorage.removeItem("isAuth");
       localStorage.removeItem("user");
       return {...state, errors: action.data, token: null, user: null,
         isAuthenticated: false, isLoading: false};
