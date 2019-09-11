e542225842bc47afcf4c4844a83e548764486c33
Darío Cáceres <dignaciocaceres@gmail.com>
2019-09-11 15:09:27 -0300

    avances en dashboard curador

--- static/src/actions/curador.js
+++ static/src/actions/curador.js
@@ -60,10 +60,65 @@ export const editCategory = () => {
 
 }
 
-export const deleteCategories = () => { return (dispatch, getState) => {
+export const deleteCategories = (auth, catArray) => { return (dispatch, getState) => {
+    console.log(catArray.length)
+    const to_send = catArray.length;
+    var completed_ok = 0;
+    var completed_err = 0;
+    dispatch({type: 'LOADING', data: ""})
+    catArray.forEach((e) => {
+      let headers = {"Authorization" : "Token "+auth,"Content-Type": "application/json"}
+      fetch('/api/categories/'+e,{method: 'DELETE', headers: headers}).then(response => {
+        if(response.status == 204){
+          ++completed_ok;
+        } else {
+          ++completed_err;
+        }
+        if (completed_err+completed_ok == to_send){
+          console.log("done")
+          console.log("errors: "+completed_err)
+          console.log("ok: "+completed_ok)
+          dispatch({type:'COMPLETED', data:""})
+          dispatch({type:'REFRESH', data:""})
+          dispatch({type:'DUMMY', data:""})
+        }
+      })
+    })
+  }}
 
-}
-  //hacer llamado a la API, si OK, entonces borrar cat de las props usando el reducer apropiado
-}
 
-//va a faltar poner aca el action de getCategories porque no se va a poder llamar al del home.
+export const getPhotos = (auth) => { return (dispatch, getState) => {
+    let headers = {"Content-Type": "application/json", "Authorization": "Token "+auth};
+
+    return fetch('/api/photos/',{method: 'GET', headers: headers})
+      .then(function(response) {
+        console.log(response)
+        const r = response
+        if(r.status === 200){
+          return r.json().then(data => {
+            dispatch({type: 'RECOVERED_PHOTOS', data: data })
+          })
+        }else{
+          dispatch({type: 'EMPTY_PHOTOS', data: r.data })
+          throw r.data
+        }
+      })
+  }}
+
+export const switchPhotoApproval = (auth, photoID, curr_value) => { return (dispatch, getState) => {
+    let headers = {"Content-Type": "application/json", "Authorization": "Token "+auth};
+    let sent_data = JSON.stringify({approved:!curr_value})
+    return fetch('/api/photos/'+photoID+'/',{method: 'PUT', headers: headers, body:sent_data})
+      .then(function(response) {
+        console.log(response)
+        const r = response
+        if(r.status === 200){
+          return r.json().then(data => {
+            dispatch({type: 'SWITCHED_PHOTO', data: data })
+          })
+        }else{
+          dispatch({type: 'ERROR', data: r.data })
+          throw r.data
+        }
+      })
+  }}
