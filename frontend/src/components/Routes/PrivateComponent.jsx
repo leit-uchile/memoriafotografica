3fa65efe1d7e9b6a9e54782cef48dd6f7c957c17
dario617 <spam.dario@gmail.com>
2020-02-26 18:34:59 -0300

    PrivateComponent redirect on route doesn match fix

--- frontend/src/components/Routes/PrivateComponent.jsx
+++ frontend/src/components/Routes/PrivateComponent.jsx
@@ -5,7 +5,8 @@ import { connect } from "react-redux";
 
 const PrivateComponent = ({ component: Component, setLoginSuccessRoute, ...rest }) => {
 
-  if (rest.isAuthenticated === null || rest.isAuthenticated === false) {
+  if (rest.isAuthenticated === null || rest.isAuthenticated === false && rest.location.pathname.includes(rest.path)) {
+    console.log(rest.location,rest.match,rest.path)
     setLoginSuccessRoute(rest.location.pathname);
     return <Redirect to="/login" />;
   }
