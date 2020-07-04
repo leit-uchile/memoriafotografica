ec3d122c437684998f48b312d670ee1b67d78599
dario617 <spam.dario@gmail.com>
2020-07-04 13:03:39 -0400

    Allow HTTP Options on all requests for CORS, django-cors handlers

--- backend/MemoriaFotografica/middleware.py
+++ backend/MemoriaFotografica/middleware.py
@@ -6,9 +6,9 @@ def dev_cors_middleware(get_response):
         def middleware(request):
             response = get_response(request)
 
-            response['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+            response['Access-Control-Allow-Origin'] = 'http://localhost'
             response['Access-Control-Allow-Methods'] = 'GET, POST, PUT, PATCH, OPTIONS, DELETE, HEAD'
-            response['Access-Control-Allow-Headers'] = 'Content-Type, X-CSRFToken'
+            response['Access-Control-Allow-Headers'] = 'Content-Type, X-CSRFToken, Authorization'
             response['Access-Control-Allow-Credentials'] = 'true'
             return response
         return middleware
