f1fa004c630c0951adf6d7a5969b1dd6e9309f65
Alejandra Alarcón <alalarco@dcc.uchile.cl>
2020-10-10 14:58:11 -0300

    Added upload success page

---
+++ frontend/src/pages/Upload/UploadSuccess.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+import {
+  Container,
+  Button,
+  Row,
+  Col,
+  Progress,
+  Alert,
+  ButtonGroup,
+} from "reactstrap";
+import "./styles.css";
+import "./uploadProgress.css";
+
+const UploadSuccess = () => {
+    return (
+      <Container>
+        <Row>
+          <Col>
+              <h2 className="page-title">
+                  Fotos subidas con &eacute;xito
+              </h2>
+          </Col>
+        </Row>
+        <Row style={{ marginTop: "2em"}}>
+          <Col md={{ size: 6, offset: 3}}>
+            <h4>
+                Tus fotos deber&aacute;n ser aprobadas 
+                por nuestro equipo de curadores. 
+                Cuando esto ocurra tus fotos 
+                estar&aacute;n disponibles para la comunidad
+            </h4>
+          </Col>
+        </Row>
+        <Row>
+          <h2 className="page-title">Pasos a seguir</h2>
+        </Row>
+        <Row xs="3">
+          <Col className="text-center">
+          <img src="/assets/cropping-tool.svg" width="30%"/>
+            <h3>Editar fotos</h3>
+          </Col>
+          <Col className="text-center">
+            <img src="/assets/photo-album.svg" width="30%"/>
+            <h3>Crear album</h3>
+          </Col>
+          <Col className="text-center">
+          <img src="/assets/image-file.svg" width="30%"/>
+          <h3>¡Subir más!</h3>
+          </Col>
+        </Row>    
+      </Container>
+    );
+}
+
+export default UploadSuccess; 
