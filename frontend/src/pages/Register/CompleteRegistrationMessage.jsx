b42d6ebe3db43a4ba3791d47c209abf1956cffa2
Joaquin Diaz <joaquin.diaz.t@ug.uchile.cl>
2021-04-10 13:43:56 -0400

    Updating message response from frontend

--- frontend/src/pages/Register/CompleteRegistrationMessage.jsx
+++ frontend/src/pages/Register/CompleteRegistrationMessage.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import "../../components/Routes/noMatch.css"
+import {Link} from 'react-router-dom';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useHistory } from "react-router-dom";
 import {
@@ -28,14 +30,14 @@ import {
   faUserTag,
 } from "@fortawesome/free-solid-svg-icons";
 import FormTermsOfUse from "../../components/TermsOfUse/FormTermsOfUse";
+import {FailedConfirmation} from "./EmailConfirmation"
 
 export default function CompleteRegistrationMessage  ({
     status
 })  {
 
     return(
-        <>HOLA {status}</>
-        
+        <FailedConfirmation/>
   )
 };
 
