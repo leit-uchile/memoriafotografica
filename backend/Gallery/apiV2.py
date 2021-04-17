f28bf7ff143c4e10a98e2e9105f86440d9366d70
Darío Cáceres <dignaciocaceres@gmail.com>
2021-04-17 12:16:32 -0400

    feat: add Curator permission check to users in v2/photos API

--- backend/Gallery/apiV2.py
+++ backend/Gallery/apiV2.py
@@ -3,21 +3,38 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from .models import Photo
 from .serializers import PhotoSerializerV2
-
+from .permissions import UserObjectPermission
 
 class PhotoViewSet(viewsets.ModelViewSet):
     serializer_class = PhotoSerializerV2
+    permission_classes = [UserObjectPermission,]
 
     def get_queryset(self):
-        # get request user 
+        # get request user
         user = self.request.user
-        # retrieve permissions if user is curator
-        if user.is_curator:
-            return Photo.objects.all()
-        # if collaborator can only PUT/PATCH/DELETE its photos
-        else:
-            if self.request.method in ['GET', 'POST']:
-                return Photo.objects.all()
-            return user.photos.all()
+        queryset = None
+
+        # public methods
+        if self.request.method in ['GET', 'POST']:
+            queryset = Photo.objects.all()
+        
+        # if user is curator check current permissions
+        # Note: if user has flag 'is_superuser' implicitly will have every permission for every object registered in the app
+        # so user.has_perm() will always be true.
+        if(not user.is_anonymous):
+            # retrieve permissions if user is curator
+            if user.is_curator and user.has_perm('Gallery.change_photo'):
+                queryset = Photo.objects.all()
+            if user.is_curator and user.has_perm('Gallery.delete_photo'):
+                queryset = Photo.objects.all()
+
+        # if user is collaborator can only PUT/PATCH/DELETE its photos
+        if(queryset == None): queryset = user.photos.all()
+        return queryset.order_by('-id')
+
+
+
+
+        
             
 
--- backend/Gallery/permissions.py
+++ backend/Gallery/permissions.py
@@ -2,25 +2,12 @@ from rest_framework import permissions
 from Users.models import User
 
 
-class IsOwner(permissions.BasePermission):
-    """
-    Object-level permission to only allow owners of an object to edit it.
-    Assumes the model instance has an `owner` attribute.
-    """
+class UserObjectPermission(permissions.BasePermission):
 
-    def has_object_permission(self, request, view, obj):
-        # Read permissions are allowed to any request,
-        # so we'll always allow GET, HEAD or OPTIONS requests.
-            # Instance must have an attribute named `owner`.
-
-        return obj.user_set.first() == request.user
-
-class FilterContent(permissions.BasePermission):
     def has_permission(self, request, view):
-        #Leer los permisos asignados al usuario
-        user_type = request.user.user_type
-        try:
-            c = request.data['censure']
-            return user_type != 1
-        except KeyError:
+        print(request.method)
+        if request.method in ['GET', 'POST']:
             return True
+        if(not request.user.is_anonymous):
+            return True
+        return False
