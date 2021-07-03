c99c1b1a9350661ddb6604eb74e30cc8a705452d
Alejandra Alarcón <alalarco@dcc.uchile.cl>
2021-07-03 14:00:57 -0400

    Remove snowball filter from analyzer

--- backend/Search_indexes/documents/analyzers.py
+++ backend/Search_indexes/documents/analyzers.py
@@ -4,6 +4,6 @@ from elasticsearch_dsl import analyzer
 html_strip = analyzer(
     'html_strip',
     tokenizer="standard",
-    filter=["lowercase", "stop", "snowball"],
+    filter=["lowercase", "stop"],
     char_filter=["html_strip"]
 )
