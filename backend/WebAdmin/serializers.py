dbc84192da96322fb97b2221e18c67a662f76ada
IsaiasVenegas <isaias.venegas@ing.uchile.cl>
2021-07-25 00:16:52 -0400

    TicketSerializer created

--- backend/WebAdmin/serializers.py
+++ backend/WebAdmin/serializers.py
@@ -1,11 +1,9 @@
 # Import models here
-from .models import News, LandingCaroussel, PhotoRequest, ContactRequest
-
+from .models import News, LandingCaroussel, PhotoRequest, ContactRequest, Ticket
 from rest_framework import serializers
 from Gallery.serializers import PhotoSerializer
 from django.conf import settings
 from datetime import datetime
-# Create serializers here :)
 
 
 class NewsSerializer(serializers.ModelSerializer):
@@ -47,5 +45,22 @@ class ContactRequestSerializer(serializers.ModelSerializer):
         instance.resolved = validated_data.get('resolved', instance.resolved)
         instance.email_sent = validated_data.get('email_sent', instance.email_sent)
         instance.reply = validated_data.get('reply', instance.reply)
+        instance.save()
+        return instance
+
+class TicketSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ticket
+        fields = '__all__'
+
+    def create(self, validated_data):
+        ticket = Ticket.objects.create(**validated_data)
+        print("ticket creado")
+        return ticket
+
+    def update(self, instance, validated_data):
+        instance.assigned = validated_data.get('assigned', instance.assigned)
+        instance.resolved = validated_data.get('resolved', instance.resolved)
+        instance.updated_at = datetime.now()
         instance.save()
         return instance
